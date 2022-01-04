85671249eda6e588876fab810d5b10e97dc10c6a
sivasubramani09 <36350531+sivasubramani09@users.noreply.github.com>
2022-01-04 08:14:02 +0530

    add BookGenre.java

---
+++ src/main/java/com/semanticsquare/thrillio/constants/BookGenre.java
@@ -0,0 +1,25 @@
+package com.semanticsquare.thrillio.constants;
+
+public enum BookGenre {
+	ART("Art"),
+	BIOGRAPHY("Biography"),
+	CHILDREN("Children"),
+	FICTION("Fiction"),
+	HISTORY("History"),
+	MYSTERY("Mystery"),
+	PHILOSOPHY("Philosohy"),
+	RELIGION("Religion"),
+	ROMANCE("Romance"),
+	SELF_HELP("Self help"),
+	TECHNICAL("Technical");
+
+	private String name;
+	
+	private BookGenre(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
